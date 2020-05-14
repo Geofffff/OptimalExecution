d35c8ee080521a349c908050d561c3d56da7496c
Toby Weston <tobyweston94@gmail.com>
2020-05-14 09:40:29 +0100

    Initial Version

---
+++ Library/local_environments.py
@@ -0,0 +1,56 @@
+from numpy import exp
+    
+class agent_environmentM:
+
+    def __init__(self, market, position,num_steps,terminal,action_values_pct,n_strats):
+        self.m = market
+        self.initial = np.ones(n_strats) * position
+        self.n_strats = n_strats
+        self.reset()
+        self.terminal = terminal
+        self.step_size = terminal / num_steps
+        # Possible amounts to sell: 0 - 10% of the total position
+        self.action_values = np.array(action_values_pct) * position 
+        self.num_actions = len(self.action_values)
+        self.state_size = 2
+
+
+    def sell(self,volumes):
+        capped_volume = np.minimum(volumes,self.position)
+        self.position -= capped_volume
+        returns = self.m.sell(capped_volume,self.step_size) 
+        self.cash += returns
+        return returns
+
+    def reset(self):
+        self.position = self.initial.copy()
+        self.cash = np.zeros(self.n_strats)
+        self.time = 0
+        self.m.reset()
+        return self.state() # State not dynamic (full = False)
+
+    def progress(self,dt):
+        self.m.progress(dt)
+        self.time += dt
+
+    def state(self,full = False):
+
+        times = np.ones(self.n_strats) * self.time
+        # TODO: Store state as a seprate variable
+        states = np.vstack((self.position,times))
+        
+        return np.vstack((self.position,times)).T
+    
+    def step(self,actions):
+        self.progress(self.step_size)
+        
+        rewards = self.sell(self.action_values[actions])
+        done = (self.position == 0) + (self.time >= self.terminal)
+        done = np.array(done,dtype = bool)
+            
+		# Reward is currently just the returned cash / 100...
+		# Not sure what the last value of the tuple should be??
+        
+        # Ufortunately this is no longer the format of the AI gym env
+        # Ideally this would be flexible depending on the input (array vs scalar)
+        return self.state(), rewards, done
---
+++ Library/market_modelsM.py
@@ -0,0 +1,82 @@
+from random import gauss
+from numpy import exp
+from numpy import ones
+from numpy import vectorize
+
+class stock:
+	
+	def __init__(self, initial, drift, vol):
+		self.initial = initial
+
+
+
+
+class bs_stock:
+
+	def __init__(self, initial, drift, vol):
+		self.initial = initial
+		self.price = initial
+		self.drift = drift
+		self.vol = vol
+
+	def generate_price(self,dt,St = None):
+		if St is None:
+			St = self.price
+
+		self.price = St * exp((self.drift - 0.5 * self.vol) * dt + self.vol * dt**0.5 * gauss(0,1))
+		return self.price
+
+	def generate_path(self,T,grid_size):
+		res = []
+		self.reset()
+		next_price = self.price
+		res.append(next_price)
+		dt = T / grid_size
+
+		for i in range(grid_size):
+			next_price = generate_price(dt)
+			res.append(next_price)
+
+	def reset(self):
+		self.price = self.initial
+
+	def __str__(self):
+		print(f"Stock Price: {self.price} \n \
+		 Black Scholes Dynamics, drift: {self.drift}, vol: {self.vol}")
+
+
+class market:
+	'''Basic market model, base class for more complex models'''
+
+	def __init__(self,stock_,num_strats = 1):
+		self.stock = stock_
+		self.spread = 0
+		self.price_adjust = ones(num_strats)
+
+
+	def sell(self,volume,dt):
+		'''sell *volume* of stock over time window dt, volume is np array'''
+		self.price_adjust *= vectorize(self.exp_g)(volume)
+		#print("volume ", volume, "price_adjust ",self.price_adjust)        
+		ret = (self.stock.price * self.price_adjust - vectorize(self.f)(volume/dt) - 0.5 * self.spread) * volume 
+		return ret
+
+
+	def g(self,v):
+		return v * 0.001
+    
+	def exp_g(self,v):
+		return exp(-self.g(v))
+
+	def f(self,v):
+		return v * 0.001
+
+	def reset(self):
+		self.stock.reset()
+		self.price_adjust = ones(len(self.price_adjust))
+
+	def progress(self,dt):
+		self.stock.generate_price(dt)
+
+	def state(self):
+		return (self.stock.price)
