Major rework of QRA QRAgent, added reward scaling, fixed wandb config in simulator

library/agents/baseAgents.py
@@ -129,11 +129,15 @@ class learningAgent:
 		self.tree_n = tree_horizon
 		self.learning_rate = 0.001
 		self.gamma = 1
+		self.reward_scaling = True
 
 		# Market data (currently only prices)
 		self.market_data_size = market_data_size
 		if self.market_data_size > 0:
 			self.stock_model = self._build_stock_model(market_data_size)
+
+		# Switch for agent evaluation mode
+		self.evaluate = False ### TODO!!!!!
 		
 		self.model = self._build_model()
 
@@ -185,7 +189,7 @@ class learningAgent:
 		# Note this applies only to epsilon greedy algorithms
 
 		# Epsilon case
-		if np.random.rand() <= self.epsilon:
+		if np.random.rand() <= self.epsilon and not self.evaluate:
 			rand_act = random.randrange(self.action_size)
 			return rand_act
 		

library/agents/distAgentsWIP2.py
@@ -45,27 +45,39 @@ class distAgent(learningAgent):
 
 	def act(self, state):
 		# No eps greedy required for 'UCB' type update
+
+		# Predict return
+		act_values = self.predict(state)
+
+		if self.evaluate:
+			return np.argmax(act_values[0])
+		
 		if self.UCB:
-			if self.t < 10:
+			if self.t < 50:
 				act = random.randrange(self.action_size)
 			else:
 				self.ct = self.c * np.sqrt(np.log(self.t) / self.t)
-				act_values = self.predict(state)
+				#act_values = self.predict(state)
+				#print("var",self.variance(state))
+				#print("act_values",act_values)
 				act = np.argmax(act_values[0] + self.ct * np.sqrt(self.variance(state)))
+				#print("Act",act)
 			return act
 		# random action
 		if np.random.rand() <= self.epsilon:
 			rand_act = random.randrange(self.action_size)
 			return rand_act#random.randrange(self.action_size)
-		# Predict return
-		act_values = self.predict(state)		
+				
 		return np.argmax(act_values[0])
 
 	# THIS FUNCTION HAS REPLACED TRASNFORM ACTION - NEEDS PROPIGATING AND TESTING
 	# PROBABLY ALSO NEED TO RESHAPE NEW MARKET STATE
 	def _process_state_action(self,state,action_index):
 		#print("state",state)
-		local_state, market_state = state
+		if self.market_data_size > 0:
+			local_state, market_state = state
+		else:
+			local_state = state
 
 		action = self.action_values[action_index] * self.trans_a + self.trans_b
 		local_state_action = np.reshape(np.append(local_state,action), [1, len(local_state[0]) + 1])
@@ -85,8 +97,8 @@ class distAgent(learningAgent):
 class C51Agent(distAgent):
 
 	def __init__(self,state_size, action_values, agent_name,N=51,C = 0,alternative_target = False,UCB = False,UCBc = 1,tree_horizon = 3,market_data_size=0):
-		self.V_max = 0.06
-		self.V_min = -0.06
+		self.V_max = 0.05
+		self.V_min = -0.1
 
 		self.N = N # This could be dynamic depending on state?
 		# This granularity is problematic - can we do this without discretisation?
@@ -95,7 +107,6 @@ class C51Agent(distAgent):
 		self.dz = (self.V_max - self.V_min) / (self.N - 1) # (2 * self.V_max)
 		self.z = np.array(range(self.N)) * (self.V_max - self.V_min) / (self.N - 1) + self.V_min
 		
-		self.reward_mapping = True # Purely for Wandb config purposes 
 		distAgent.__init__(self,state_size, action_values, agent_name,C, alternative_target,UCB,UCBc,tree_horizon,market_data_size=market_data_size)
 
 	def return_mapping(self,state,ret,inverse = False):
@@ -135,13 +146,13 @@ class C51Agent(distAgent):
 		return Tz
 
 	def mapped_z(self,state):
-		return self.z + (state[0][0][0] / 2 + 0.5)
+		return self.z + (state[0][0] / 2 + 0.5)
 
 	def mapped_dz(self,state):
 		return self.dz
 
 	def mapped_bounds(self,state):
-		return (self.V_min + (state[0][0][0] / 2 + 0.5), self.V_max + (state[0][0][0] / 2 + 0.5))
+		return (self.V_min + (state[0][0] / 2 + 0.5), self.V_max + (state[0][0] / 2 + 0.5))
 
 	# Get unmapped results using basis: STATE
 	# Could be made more efficient
@@ -272,7 +283,7 @@ class IQNNetwork(Model):
 	def call(self,inputs):
 		
 		state_action,quantiles_selected = inputs
-		embedded_quantiles = np.cos(np.dot(embedded_range, self.quantiles_selected) * np.pi)
+		embedded_quantiles = np.cos(np.dot(embedded_range, self.quantiles) * np.pi)
 		embedded_quantiles.shape = (self.N,1)
 		processed_state = self.process_state(state_action)
 		combined = self.main_hidden1([processed_state,embedded_quantiles])
@@ -293,7 +304,7 @@ class CosineBasisLayer(Layer):
 
 	def call(self, inputs):
 		# Should these lines be converted to TF?
-		#quantiles.shape = (1,len(self.quantiles_selected))
+		#quantiles.shape = (1,len(self.quantiles))
 
 		embedding_range = K.arange(1,self.units + 1) # Note this is DIFFERENT to paper
 		# dot does not do automatic type conversion so...
@@ -309,36 +320,38 @@ class CosineBasisLayer(Layer):
 
 # Temporarily swtiched to QRAgent
 class QRAgent(distAgent):
-	def __init__(self,state_size, action_values, agent_name,C, alternative_target = False,UCB=False,UCBc = 1,tree_horizon = 3):
-		self.N = 3
+	def __init__(self,state_size, action_values, agent_name,C, alternative_target = False,UCB=False,UCBc = 1,tree_horizon = 3,market_data_size=0):
+		self.N = 32
 		self.N_p = 8
 		self.embedding_dim = 3
 		self.state_model_size_out = 8
 		#self.kappa = 2 # What should this be? Moved to loss fun
-		self.selected_qs = None
-
-		self.embedded_range = np.arange(self.embedding_dim) + 1 # Note Chainer and dopamine implementation
-		self.embedded_range.shape = (self.embedding_dim,1)
+		#self.selected_qs = None
+		#self.embedded_range = np.arange(self.embedding_dim) + 1 # Note Chainer and dopamine implementation
+		#self.embedded_range.shape = (self.embedding_dim,1)
 
 		# Temporarily uniformly parition [0,1] for the quantiles
-		self.quantiles_selected = np.arange(1,self.N + 1) / (self.N+1) # np.random.rand(self.N) This should be a random partition of [0,1]
-		#print(self.quantiles_selected)
-		self.qi = 1 / (self.N)#self.quantiles_selected[1] - self.quantiles_selected[0] # WARNING: SHOULD BE DYNAMIC
-		self.quantiles_selected.shape = (1,len(self.quantiles_selected))
-		self.embedded_quantiles = np.cos(np.dot(self.embedded_range, self.quantiles_selected) * np.pi)
-		self.embedded_quantiles.shape = (1,self.embedding_dim,self.N)
+		self.quantiles = np.arange(1,self.N + 1) / (self.N+1) # np.random.rand(self.N) This should be a random partition of [0,1]
+		#print(self.quantiles)
+		self.qi = 1 / (self.N)#self.quantiles[1] - self.quantiles[0] # WARNING: SHOULD BE DYNAMIC
+		self.quantiles.shape = (1,len(self.quantiles))
+		#self.embedded_quantiles = np.cos(np.dot(self.embedded_range, self.quantiles) * np.pi)
+		#self.embedded_quantiles.shape = (1,self.embedding_dim,self.N)
 		self.kappa = 1
-		super(QRAgent,self).__init__(state_size, action_values, agent_name,C, alternative_target,UCB,UCBc,tree_horizon)
+		self.optimisticUCB = False
+		super(QRAgent,self).__init__(state_size, action_values, agent_name,C, alternative_target,UCB,UCBc,tree_horizon,market_data_size)
 			
 	# https://stackoverflow.com/questions/55445712/custom-loss-function-in-keras-based-on-the-input-data
 	@staticmethod
 	def huber_loss_quantile(tau,kappa):
 		#kappa = 2
 		def loss(yTrue,yPred):
-			bellman_errors =   yPred - yTrue
+			bellman_errors =   yTrue - yPred
 			#tau = np.array(quantile_in)
 			#print("loss",(K.abs(tau - K.cast(bellman_errors < 0,"float32")) * huber_loss(yTrue,yPred)) / kappa)
-			return (K.abs(tau - K.cast(bellman_errors < 0,"float32")) * huber_loss(yTrue,yPred)) / kappa
+			if kappa > 0:
+				return (K.abs(tau - K.cast(bellman_errors < 0,"float32")) * huber_loss(yTrue,yPred)) / kappa
+			return (K.abs(tau - K.cast(bellman_errors < 0,"float32")) * bellman_errors)
 		return loss
 		
 
@@ -346,8 +359,8 @@ class QRAgent(distAgent):
 		# Using Keras functional API
 		
 		state_in = Input(shape=(self.state_size + 1,))
-		state_hidden1 = Dense(8, activation='relu')(state_in)
-		state_hidden2 = Dense(self.state_model_size_out, activation='relu')(state_hidden1)
+		state_hidden1 = Dense(32, activation='relu')(state_in)
+		state_hidden2 = Dense(32, activation='relu')(state_hidden1)
 		#hidden3 = Dense(30, activation='relu')(hidden2)
 		
 
@@ -361,11 +374,16 @@ class QRAgent(distAgent):
 		
 		# Full Model
 		#main_hidden1 = Multiply()([cosine_layer, state_hidden2])
-		main_hidden2 = Dense(30, activation='relu')(state_hidden2) #main_hidden1
+		main_hidden2 = Dense(32, activation='relu')(state_hidden2) #main_hidden1
 		outputs = Dense(self.N, activation='linear')(main_hidden2)
-		main_model = Model(inputs=state_in, outputs=outputs)
+		#main_model = Model(inputs=state_in, outputs=outputs)
 
-		main_model.compile(loss = self.huber_loss_quantile(self.quantiles_selected,self.kappa),
+		if self.market_data_size > 0:
+			model = Model(inputs=[state_in,self.stock_model.input], outputs=outputs)
+		else:
+			model = Model(inputs=state_in, outputs=outputs)
+
+		model.compile(loss = self.huber_loss_quantile(self.quantiles,self.kappa),
 						optimizer=Adam(lr=self.learning_rate))
 		'''
 		main_model = IQNNetwork(self.state_size + 1,self.N,self.state_model_size_out,self.embedding_dim)
@@ -373,74 +391,116 @@ class QRAgent(distAgent):
 		main_model.compile(loss = self.huber_loss_quantile(quantiles_selected,self.kappa),
 						optimizer=Adam(lr=self.learning_rate))
 		'''
-		return main_model
+		return model
 
-	def predict_action(self,state,action_index,quantiles_selected,target = False):
-		#print("quantiles",self.quantiles_selected)
-		#print(np.dot(self.embedded_range, self.quantiles_selected))
+	def predict_action(self,state,action_index,target = False,above_med = False):
+		#print("quantiles",self.quantiles)
+		#print(np.dot(self.embedded_range, self.quantiles))
 		#print("action index",action_index)
 		state_action = self._process_state_action(state,action_index)
 		#quantile_in = self.process_quantiles(quantiles_selected)
 		#print("state action",state_action,"embedded_quantiles",self.embedded_quantiles)
 		#print(self.model.summary())
-		if self.C > 0 and target:
-			return np.add.reduce(self.quantiles_selected * self.target_model.predict([state_action,self.quantiles_selected]),1)
-		#print("pre reduce predict_action output",np.add.reduce(self.model.predict([state_action,self.quantiles_selected]),1))
-		#print("predict_action output",self.model.predict([state_action,self.quantiles_selected]))
-
-		return np.add.reduce(self.qi * self.model.predict(state_action),1)
+		predict = self.predict_quantiles(state_action,target)
+		#if self.C > 0 and target:
+			#assert not above_med, "Why is variance being used with target?"
+			#return np.add.reduce(self.qi * predict,1)
+		#print("pre reduce predict_action output",np.add.reduce(self.model.predict([state_action,self.quantiles]),1))
+		#print("predict_action output",self.model.predict([state_action,self.quantiles]))
+		#predict = self.model.predict(state_action)[0]
+		if above_med:
+			predict = predict[0][int(len(predict)/2):]
+			np.reshape(predict,[1,len(predict)])
+			#return np.add.reduce(self.qi * predict,1)
+		return np.add.reduce(self.qi * predict,1)
 
 	# This function needs to be rolled into predict_action
-	def predict_quantiles(self,state,action_index,quantiles_selected,target = False):
-		state_action = self._process_state_action(state,action_index)
+	def predict_quantiles(self,state_action,target = False):
+		#state_action = self._process_state_action(state,action_index)
 		if DEBUG:
 			print("predict state action", state_action)
-		state_action.shape = (1,len(state_action))
+		#state_action.shape = (1,len(state_action))
 		if self.C > 0 and target:
-			return self.target_model.predict([state_action,self.quantiles_selected])
-		return self.model.predict([state_action,self.quantiles_selected])
+			return self.target_model.predict(state_action) + state_action[0][0] / 2 + 0.5
+		return self.model.predict(state_action) + state_action[0][0] / 2 + 0.5
 
 
 	# predict function could be moved to distAgent and transitioned to np
-	def predict(self,state,quantiles_selected = None,target = False):
+	def predict(self,state,target = False):
 		res = []
-		if quantiles_selected == None:
-			quantiles_selected = np.random.uniform(self.N)
 		for i in range(self.action_size):
-			res.append(self.predict_action(state,i,quantiles_selected,target = target))
+			res.append(self.predict_action(state,i,target = target))
 
 		res = np.array(res)
 		res.shape = (1,len(res))
 		return res
 
+	def project(self,reward,next_state,done,horizon,mem_index):
+		tree_success = False
+		predict = []
+		if not done:
+			next_action_index = np.argmax(self.predict(next_state)[0])
+			if horizon > 1 and mem_index < (self.memory.size - 1):
+				state1, action1, reward1, next_state1, done1 = self.memory[mem_index + 1]
+				if next_action_index == action1:
+					predict = self.project(reward1,next_state1,done1,horizon - 1,mem_index + 1)
+					tree_success = True
+			if not tree_success:
+				next_state_action = self._process_state_action(next_state,next_action_index)
+				predict = self.predict_quantiles(next_state_action,target = True)
+		else:
+			predict = np.zeros(self.N)
+			predict = np.reshape(predict, [1, self.N])
+
+		return reward + self.gamma * predict
+
 	def fit(self,state, action_index, reward, next_state, done,mem_index = -1):
-		quantiles_selected = np.random.uniform(self.N)
 		state_action = self._process_state_action(state,action_index)
 		if DEBUG:
 			print("State Action",state_action)
 		# For Double Deep
 		#print("predictions",self.predict(next_state,quantiles_selected = quantiles_selected))
-		next_action_index = np.argmax(self.predict(next_state,quantiles_selected = quantiles_selected)[0])
-		if not done:
-			target = reward + self.gamma * self.predict_quantiles(next_state,next_action_index,quantiles_selected,target = True)
-		else:
-			target = np.ones(self.N) * reward
+		
+		
+		
+
+		target = self.project(reward,next_state,done,self.tree_n,mem_index) - state[0][0] / 2 - 0.5
+		#print(target)
+
+			
 		if DEBUG:
 			print("Target", target)
 		#print("target shape",target.shape)
-		target_f = np.reshape(target, [1, self.N])
-		#print("predicted", self.predict_quantiles(state,action_index,self.quantiles_selected,target = False),"target",target_f)
-		#print("loss", self.huber_loss_quantile(self.quantiles_selected,1)(target_f,self.predict_quantiles(state,action_index,self.quantiles_selected,target = False)))
+		target_f = target #np.reshape(target, [1, self.N])
+		#print("predicted", self.predict_quantiles(state,action_index,self.quantiles,target = False),"target",target_f)
+		#print("loss", self.huber_loss_quantile(self.quantiles,1)(target_f,self.predict_quantiles(state,action_index,self.quantiles,target = False)))
 		
-		self.model.fit([state_action,self.quantiles_selected], target_f,epochs=1, verbose=0)
+		self.model.fit(state_action, target_f,epochs=1, verbose=0)
 
 
 	def process_quantiles(self,quantiles_selected):
 		# Move to class init (why reinitialise?)
 		assert False
 
+	def action_variance(self,state,action_index):
+		state_action = self._process_state_action(state,action_index)
+		predict = self.predict_quantiles(state_action)[0]
+		if self.optimisticUCB:
+			predict = predict[int(len(predict) / 2):]
+		#print("action_var",np.add.reduce(self.qi * predict * predict))
+		return np.add.reduce(self.qi * predict * predict)
+
+	def variance(self,state):
+		res = []
+		#print("00",np.power(self.predict_action(state,0,above_med = self.optimisticUCB),2))
+		for i in range(self.action_size):
+			res.append(self.action_variance(state,i) - np.power(self.predict_action(state,i,above_med = self.optimisticUCB)[0],2))
+		return res
+
 # Testing the code
 if __name__ == "__main__":
+	pass
+
 	'''
 	myAgent = distAgent(5,"TonyTester",N=5)
 	state = [-0.8,0.8] 
@@ -450,7 +510,7 @@ if __name__ == "__main__":
 	next_state = [-1,0.9] 
 	next_state = np.reshape(state, [1, 2])
 	myAgent.epsilon_min = 0.01
-	'''
+	
 	myAgent = QRAgent(2,[0.1,0.5,1.0],"TonyTester",C=0,market_data_size = 16)
 	state = [-1,1] 
 	state = np.reshape(state, [1, 2])
@@ -510,7 +570,7 @@ if __name__ == "__main__":
 		print("... and the probs:",myAgent.probs(next_state,my_next_action)[0])
 		print("resulting in projTZ ", myAgent.projTZ_nTree(state,0.1,next_state,False,0,1))
 		myAgent.fit(state,2,0.1,next_state,False)
-
+	'''
 
 
 

library/local_environments.py
@@ -15,9 +15,9 @@ class agent_environmentM:
 
         # Local environment
         self.initial_position = np.ones(n_strats) * position
-        self.reset()
         self.terminal = terminal # In seconds
         self.step_size = terminal / num_steps
+        self.reset()
         
         # Possible amounts to sell: 0 - 10% of the total position
         self.action_values = np.array(action_values_pct) * position 
@@ -32,7 +32,7 @@ class agent_environmentM:
         returns = self.m.sell(capped_volume,self.step_size) 
         self.cash += returns
         #print("time ", self.time, "capped_volume ", capped_volume, "state ", self.state(),"returns ", returns)
-        return returns
+        return returns, capped_volume
 
     def reset(self,training = True):
         self.position = self.initial_position.copy()
@@ -47,7 +47,7 @@ class agent_environmentM:
 
     def state(self,full = False):
 
-        times = np.ones(self.n_strats) * 2 * self.time - 1
+        times = np.ones(self.n_strats) * 2 * self.time / self.terminal - 1
         # TODO: Store state as a seprate variable
         res = np.vstack((2 * self.position/self.initial_position[0] - 1,times)) # Assuming initial position always the same
         if self.market_data:
@@ -57,16 +57,19 @@ class agent_environmentM:
     
     def step(self,actions):
         self.progress(self.step_size)
-        if self.time < self.terminal:
-            rewards = self.sell(self.action_values[actions])
+        time_out = (round(self.time,7) >= self.terminal)
+        if time_out:
+            rewards, amount = self.sell(self.position)
+            #print("Selling off",amount)
         else:
-            rewards = self.sell(self.position)
+            rewards, amount = self.sell(self.action_values[actions])
+            #print("selling ",amount)
         done = (self.position <= 0) + (round(self.time,7) >= self.terminal)
         if any(self.position < 0):
             print("Warning position is ",self.position)
         done = np.array(done,dtype = bool)
 
-        rewards = self.scale_rewards(rewards)
+        rewards = self.scale_rewards(rewards,amount)
         #print("times ",self.time, self.terminal)
             
 		# Reward is currently just the returned cash / 100...
@@ -77,7 +80,7 @@ class agent_environmentM:
         return self.state(), rewards, done
 
 
-    def scale_rewards(self,rewards):
+    def scale_rewards(self,rewards,amount):
         #print(type(self.m.stock.initial),self.m.stock.initial)
-        return rewards / (self.initial_position[0] ) # /* self.m.stock.initial
+        return (rewards) / (self.initial_position[0] ) # /* self.m.stock.initial
 

library/market_modelsM.py
@@ -37,7 +37,7 @@ class bs_stock:
 			next_price = generate_price(dt)
 			res.append(next_price)
 
-	def reset(self):
+	def reset(self,training=None):
 		self.price = self.initial
 
 	def __str__(self):
@@ -156,10 +156,13 @@ class real_stock:
 		return self.price
 
 	def hist_price(self,n,dt):
+		dt_adj = dt / self.data_freq
+		assert dt_adj.is_integer(), "Time step must be an integer"
+		dt_adj = int(dt_adj)
 		res = []
 		for i in range(n):
-			res.append(self.df[self.data_index - i * dt])
-		return self.df[self.data_index-i:self.data_index] / self.initial
+			res.append(self.df[self.data_index + (- n + i + 1 ) * dt_adj])
+		return np.array(res) / self.initial
 
 
 # Need to rework to record n previous prices...
@@ -167,6 +170,7 @@ class market:
 	'''Basic market model, base class for more complex models'''
 
 	def __init__(self,stock_,num_strats = 1,n_hist_prices = 0):
+		self.k = 0.00186
 		self.stock = stock_
 		self.stock.hist_buffer = n_hist_prices
 		self.spread = 0
@@ -181,6 +185,7 @@ class market:
 		self.price_adjust *= np.vectorize(self.exp_g)(volume)
 		#print("volume ", volume, "price_adjust ",self.price_adjust)        
 		ret = (self._adjusted_price() - np.vectorize(self.f)(volume/dt) - 0.5 * self.spread) * volume 
+		#print("return",ret)
 		return ret
 
 	def g(self,v):
@@ -190,7 +195,7 @@ class market:
 		return np.exp(-self.g(v))
 
 	def f(self,v):
-		return v * 0.00#0.00186 # Temporarily adjusting by 10 to account for non unit terminal
+		return v * self.k#0.00186 # Temporarily adjusting by 10 to account for non unit terminal
 		# What should this be?
 			# - HFT book (position = 1, terminal  = 1, k = 0.01)
 			# Since position = 1 but terminal = 10 I've *10
@@ -203,13 +208,14 @@ class market:
 		self.stock.reset(training)
 		self.price_adjust = np.ones(len(self.price_adjust))
 		for i in range(self.n_hist_prices):
-			self.hist_prices = self.stock.hist_price(self.n_hist_prices)
+			self.hist_prices = self.stock.hist_price(self.n_hist_prices,dt)
 		#return self.hist_prices
 
 	def progress(self,dt):
 		self.stock.generate_price(dt)
 		# MULTIPLE STRATS NOT SUPPORTED HERE
-		self.hist_prices[:-1] = self.hist_prices[1:]; self.hist_prices[-1] = self._adjusted_price()[0]
+		if self.n_hist_prices > 0:
+			self.hist_prices[:-1] = self.hist_prices[1:]; self.hist_prices[-1] = self._adjusted_price()[0]
 
 	def state(self,n_prices = 1):
 		return self.hist_prices

library/simulations.py
@@ -57,28 +57,32 @@ class simulator:
 			new_run.config.update({"num_trades": self.num_steps,
 			 "batch_size": self.batch_size,
 			 "action_size": len(self.possible_actions),
-			 "state_size": self.env.state_size
+			 "state_size": self.env.state_size,
+			 "temp_impact": self.env.m.k,
+			 "stock": type(self.env.m.stock).__name__
 			 })
 			if agent.agent_type != "basic":
-				new_run.config.target_lag: agent.C
-				new_run.config.alt_target: agent.alternative_target
-				new_run.config.tree_horizon: agent.tree_n
-				new_run.config.buffer_size: agent.replay_buffer_size
-				new_run.config.learning_rate: agent.learning_rate
+				new_run.config.update({"target_lag": agent.C,
+				 "alt_target": agent.alternative_target,
+			 	 "tree_horizon": agent.tree_n,
+			 	 "buffer_size": agent.replay_buffer_size,
+			 	 "learning_rate": agent.learning_rate,
+			 	 "reward_scaling": agent.reward_scaling
+			 	})
 				
-				if agent.UCB:
-					new_run.config.UCBc = agent.c
-				else:
-					new_run.config.epsilon_min = agent.epsilon_min
-					new_run.config.epsilon_decay = agent.epsilon_decay
-
 			if agent.agent_type == "dist":
 				if type(agent).__name__ == "C51Agent":
-					new_run.config.reward_mapping = agent.reward_mapping
-					new_run.config.support_range = agent.V_max - agent.V_min
+					new_run.config.update({"support_range": agent.V_max - agent.V_min})
 				if type(agent).__name__ == "QRAgent":
-					new_run.config.n_quantiles = agent.N
-
+					new_run.config.update({"n_quantiles": agent.N})
+				if agent.UCB:
+					new_run.config.update({"UCBc": agent.c})
+				else:
+					new_run.config.update({"epsilon_min": agent.epsilon_min})
+					new_run.config.update({"epsilon_decay": agent.epsilon_decay})
+			else:
+				new_run.config.update({"epsilon_min": agent.epsilon_min})
+				new_run.config.update({"epsilon_decay": agent.epsilon_decay})
 
 			# Agent specifics
 			self.wandb_agents.append(new_run)
@@ -166,6 +170,7 @@ class simulator:
 		for i ,agent in enumerate(self.agents):
 			agent.epsilon = epsilon[i]
 			agent.epsilon_decay = epsilon_decay[i]
+			agent.evaluate = evaluate
 			
 		# Setup action list
 		actions = [-1] * self.n_agents
@@ -238,11 +243,13 @@ class simulator:
 	def episode(self,actions, verbose = False,evaluate = False):
 		states = self.env.reset(training = (not evaluate)) # reset state at start of each new episode of the game
 		#states = np.reshape(states, [self.n_agents,1, self.env.state_size])
-
+		track_action_p = False
 		# Log action values
 		if not evaluate:
 			self.episode_n += 1
 			if self.episode_n % self.action_record_frequency == 0:
+				track_action_p = True
+				action_tracker = []
 				for i, agent in enumerate(self.agents):
 					for j in range(len(self.possible_actions)):
 						self.wandb_agents[i].log({'episode': self.episode_n, ('act_val' + str(j)): agent.predict(states)[0][j]})
@@ -266,6 +273,9 @@ class simulator:
 			
 			next_states, rewards, done = self.env.step(actions)
 			
+			assert self.n_agents ==1,  "multiple agents not supported"
+			if track_action_p:
+				action_tracker.append(actions[0])
 			#rewards = (1 - done) * rewards
 			
 			#next_states = np.reshape(next_states, [self.n_agents,1, self.env.state_size])
@@ -279,9 +289,9 @@ class simulator:
 						agent.remember(states, actions[i], rewards[i], next_states, done[i])
 
 			if verbose:
-				print("Agent 0 predict", self.agents[0].predict(states[0]))
-				print("State[0]: ",states[0], "Actions[0]: ", actions[0], "Rewards[0]: ", rewards[0], "Next_states[0]: ", next_states[0], "Done[0]: ", done[0])
-				print("Agent 0 next predict", self.agents[0].predict(next_states[0]))
+				print("Agent 0 predict", self.agents[0].predict(states))
+				print("State[0]: ",states, "Actions[0]: ", actions[0], "Rewards[0]: ", rewards[0], "Next_states[0]: ", next_states, "Done[0]: ", done[0])
+				print("Agent 0 next predict", self.agents[0].predict(next_states))
 			states = next_states
 				
 			if all(done): 
@@ -301,6 +311,11 @@ class simulator:
 		if evaluate:
 			self.eval_rewards += total_reward
 
+		if track_action_p:
+			for i in range(len(self.wandb_agents)):
+				for j in range(len(self.possible_actions)):
+					self.wandb_agents[i].log({'episode': self.episode_n, ('act_count' + str(j)): action_tracker.count(j)})
+
 	def evaluate(self,n_episodes = 200,show_stats = True):
 		epsilon_old = []
 		epsilon_decay_old = []
@@ -315,6 +330,7 @@ class simulator:
 		for i, agent in enumerate(self.agents):
 			agent.epsilon = epsilon_old[i]
 			agent.epsilon_decay = epsilon_decay_old[i]
+			agent.evaluate = False
 
 		if show_stats:
 			start_iteration = len(self.eval_rewards)