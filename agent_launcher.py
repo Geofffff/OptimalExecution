785f5c8fd933476851a9e615e6c63d6c127a5476
Toby Weston <tobyweston94@gmail.com>
2020-06-17 10:24:24 +0200

    moved current dev env to agent_launcher for the cluster

--- agent_launcher.py
+++ agent_launcher.py
@@ -15,58 +15,42 @@ params = {
     "num_trades" : 10,
     "position" : 10,
     "batch_size" : 32,
-    "action_values" : [0.05,0.075,0.09,0.1,0.11,0.15,0.2] #[0.05,0.075,0.1,0.15,0.2] 
+    "action_values" : [0.09,0.095,0.1,0.105,0.11] #[0.05,0.075,0.09,0.1,0.11,0.15,0.2] #[0.05,0.075,0.1,0.15,0.2] 
 }
 state_size = 2
 action_size = len(params["action_values"])
-n_hist_prices = 32
+n_hist_prices = 16
 
 # Define Agents
-quentin = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "Quentin",C=100, alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,market_data_size=n_hist_prices)
+quentin = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "Quentin 300",C=100, alternative_target = True,UCB=True,UCBc = 300,tree_horizon = 4,market_data_size=n_hist_prices)
 brian = library.agents.distAgentsWIP2.C51Agent(state_size, params["action_values"], "Brian Appl 1hr md32",C=100, alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,market_data_size=n_hist_prices)
-tim = library.agents.baseAgents.TWAPAgent(3,"TWAP_APPL", len(params["action_values"]))
+tim = library.agents.baseAgents.TWAPAgent(2,"TWAP_APPL", len(params["action_values"]))
 #print(brian.model.summary())
 agents = [
     quentin
 ]
 quentin.learning_rate = 0.0005
 quentin.reward_scaling = True
+
 # NOTE: Cosine basis for Isabelle results in a lot of params...
 
 # Initialise Simulator
 # BS market
-'''
-simple_stock = bs_stock(1,0,0.0005) # No drift, 0.0005 vol
-simple_market = market(simple_stock,num_strats = len(agents))
-my_simulator = library.simulations.simulator(simple_market,agents,params,test_name = "QR Testing")
-'''
 
-# real stock testing
-# Retrieve data
-
-df1 = pd.read_csv("data/2020-05-04SPX_yFinance",low_memory = False) # Load .csv
-df1 = df1["Adj Close.3"][2:]
-df2 = pd.read_csv("data/2020-05-16SPX_yFinance",low_memory = False) # Load .csv
-df2 = df2["Adj Close.3"][2:]
-df3 = pd.read_csv("data/2020-05-22SPX_yFinance",low_memory = False) # Load .csv
-df3 = df3["Adj Close.3"][2:]
-df4 = pd.read_csv("data/2020-05-28SPX_yFinance",low_memory = False) # Load .csv
-df4 = df4["Adj Close.3"][2:]
-
-appl_data = pd.concat([df1,df2,df3,df4])
-print("Warning: dropping",sum(pd.isnull(appl_data)), "nan value(s)")
-appl_data = appl_data.dropna()
-appl_data = appl_data.values # Extract APPL as np array
+df = pd.read_csv("data/EURUSD-2019-01C.csv",low_memory = False, names=["Instrument", "Time", "Bid", "Ask"]) # Load .csv
+df = df["Bid"]
+print("Warning: dropping",sum(pd.isnull(df)), "nan value(s)")
+#appl_data = appl_data.dropna()
+appl_data = df.values # Extract APPL as np array
 print("Using",len(appl_data),"values")
-appl_data = appl_data.astype(float) # convert any rouge strings to floats
-appl_stock = real_stock(appl_data,n_steps = 60,recycle = True,n_train=10) # create stock - traded once per 6 minutes and recycled
+appl_data = appl_data.astype(float) # convert any rogue strings to floats
+appl_stock = real_stock(appl_data,n_steps = 10,recycle = True,n_train=100) # create stock - traded once per 6 minutes and recycled
 appl_market = market(appl_stock,n_hist_prices = n_hist_prices)
-my_simulator = library.simulations.simulator(appl_market,agents,params,test_name = "Apple Stock Testing")
-my_simulator.train(20000)
+### Micro intervals ###
+# Assume we are executing over 30s intervals, k should therefore be 120 times smaller (both vol and time / 120 but vol appears twice)
 
-# Signal market
-'''
-signal_stock = signal_stock(1,0.0005,0.0005,0.0005) # initial 1, vol 0.0005, signal vol 0.0005, signal reversion 0.0005
-signal_market = market(signal_stock,num_strats = len(agents))
-signal_simulator = library.simulations.simulator(signal_market,agents,params,test_name = "Signal Testing 1")
-'''
+appl_market.k /= 240
+my_simulator = library.simulations.simulator(appl_market,agents,params,test_name = "EURUSD Testing")
+my_simulator.train(60000)
+
+# End
