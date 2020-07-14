d896a5929760166e19c6bb336e6f322b32dd000f
Toby Weston <tobyweston94@gmail.com>
2020-07-14 15:26:27 +0200

    cluster changes

--- library/agents/distAgentsWIP2.py
+++ library/agents/distAgentsWIP2.py
@@ -23,7 +23,7 @@ else:
 # Not required if not running on the cluster
 # Instead replace with 
 # from keras.losses import huber_loss
-local = True
+local = False
 if not local:
 	def huber_loss(y_true, y_pred, clip_delta=1.0):
 	  error = y_true - y_pred
