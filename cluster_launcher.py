11de60f9461266191825bcd769b8548aecbf8eb1
Toby Weston <tobyweston94@gmail.com>
2020-07-14 15:34:33 +0200

    Fixes

--- cluster_launcher.py
+++ cluster_launcher.py
@@ -4,12 +4,12 @@ import library.market_modelsM
 import library.agents.distAgentsWIP2, library.simulations2, library.agents.baseAgents
 
 
-df = pd.read_csv("cluster_data/cluster_BTX_19.csv",low_memory = False)
+merged = pd.read_csv("cluster_data/cluster_BTX_19.csv",low_memory = False)
 
 
 params = {
     "terminal" : 1,
-    "num_trades" : 10,
+    "num_trades" : 30,
     "position" : 10000,
     "batch_size" : 32,
     "action_values" : [[0.5,0],[1,0],[2,0],
@@ -18,12 +18,12 @@ params = {
                        [0,3],[1,3]]
 }
 state_size = 7
-harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "CX Harry11 U 10p10",C=100, alternative_target = True,UCB=True,UCBc = 200,tree_horizon = 5,orderbook =True)#,market_data_size=n_hist_prices)
+harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "CX Harry11 U 10p10",C=80, alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 15,orderbook =True)#,market_data_size=n_hist_prices)
 #tim = library.agents.baseAgents.TWAPAgent(1,"TWAP",9)
 agent = harry
 
 
-stock = library.market_modelsM.real_stock_lob(merged,n_steps=10,n_train=300)
+stock = library.market_modelsM.real_stock_lob(merged,n_steps=30,n_train=300)
 market = library.market_modelsM.lob_market(stock,0)
 market.k *= 10
 
