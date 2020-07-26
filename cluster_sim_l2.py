e6efbd9c10e8eb53523e9a7e70f259362ab7d444
Toby Weston <tobyweston94@gmail.com>
2020-07-26 14:49:17 +0200

    DDQN cluster

--- cluster_sim_l2.py
+++ cluster_sim_l2.py
@@ -1,10 +1,10 @@
-import library.agents.distAgentsWIP2, library.simulations2, library.agents.baseAgents, library.market_modelsM
+import library.agents.distAgentsWIP2, library.simulations2, library.agents.baseAgents, library.market_modelsM, library.agents.valueAgents
 
 lr = 0.00015
 ucbc = 80
 th = 4
 tl = 50
-N = 11
+N = 51
 
 params = {
     "terminal" : 1,
@@ -15,15 +15,17 @@ params = {
                         # [0.001,0.005,0.01,0.015,0.02,0.03,0.05,0.075,0.1,0.15,0.2,0.25,0.3,0.35]
 }
 state_size = 2
-harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], f"10s lr {lr} UCBc {ucbc} tl {tl} th {th} N{N}",C=tl, ,N=N,alternative_target = True,UCB=True,UCBc = ucbc,tree_horizon = th,n_hist_data=0,n_hist_inputs=0,orderbook =False)#,market_data_size=n_hist_prices)
-tim = library.agents.baseAgents.TWAPAgent(7,"TWAP",21)
-agent = harry
-agent.learning_rate = lr
+harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "Sim QR LK",C=tl,N=N, alternative_target = True,UCB=True,UCBc = ucbc,tree_horizon = th,n_hist_data=0,n_hist_inputs=0,orderbook =False)#,market_data_size=n_hist_prices)
+alice = library.agents.valueAgents.DDQNAgent(state_size, len(params["action_values"]), "Sim DDQN LK",C=tl, alternative_target = True,tree_horizon=th)
+tim = library.agents.baseAgents.TWAPAgent(5,"TWAP",21)
+agent = alice
+#agent.learning_rate = lr
 
 
 simple_stock = library.market_modelsM.bs_stock(1,0,0.0005,n_steps = 10) # No drift, 0.0005 vol
 simple_market = library.market_modelsM.market(simple_stock)
-simple_market.k = 0.003
+simple_market.k = 0.02
+simple_market.b = 0.001
 
-my_simulator = library.simulations2.simulator(simple_market,agent,params,test_name = "Test3",orderbook = False)
-my_simulator.train(20000,epsilon_decay =0.9999)
+my_simulator = library.simulations2.simulator(simple_market,agent,params,test_name = "SimulatedResults",orderbook = False)
+my_simulator.train(100000,epsilon_decay =0.9999)
