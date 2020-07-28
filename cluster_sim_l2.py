83b9d289370277588173927fdf86cd8d822e8ad1
Toby Weston <tobyweston94@gmail.com>
2020-07-28 21:34:18 +0200

    new testing agents

--- cluster_sim_l2.py
+++ cluster_sim_l2.py
@@ -1,7 +1,7 @@
 import library.agents.distAgentsWIP2, library.simulations2, library.agents.baseAgents, library.market_modelsM, library.agents.valueAgents
 
-lr = 0.00005
-ucbc = 100
+lr = 0.00001
+ucbc = 150
 th = 4
 tl = 50
 N = 200
@@ -15,7 +15,7 @@ params = {
                         # [0.001,0.005,0.01,0.015,0.02,0.03,0.05,0.075,0.1,0.15,0.2,0.25,0.3,0.35]
 }
 state_size = 2
-harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "Sim QR LK Slr",C=tl,N=N, alternative_target = True,UCB=True,UCBc = ucbc,tree_horizon = th,n_hist_data=0,n_hist_inputs=0,orderbook =False)#,market_data_size=n_hist_prices)
+harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "Sim QR LK VSlr",C=tl,N=N, alternative_target = True,UCB=True,UCBc = ucbc,tree_horizon = th,n_hist_data=0,n_hist_inputs=0,orderbook =False)#,market_data_size=n_hist_prices)
 alice = library.agents.valueAgents.DDQNAgent(state_size, len(params["action_values"]), "Sim DDQN LK Slr",C=tl, alternative_target = True,tree_horizon=th)
 tim = library.agents.baseAgents.TWAPAgent(5,"TWAP",21)
 agent = harry
