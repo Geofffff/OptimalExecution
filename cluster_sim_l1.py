de827e1206519d272f0463e6e0ecc1230cf8e356
Toby Weston <tobyweston94@gmail.com>
2020-08-05 10:29:17 +0200

    cluster

--- cluster_sim_l1.py
+++ cluster_sim_l1.py
@@ -7,23 +7,23 @@ reload(library.agents.distAgentsWIP2)
 reload(library.simulations2)
 reload(library.market_modelsM)
 
-n_hist_data = 128
+n_hist_data = 32
 
 params = {
     "terminal" : 1,
-    "num_trades" : 10,
+    "num_trades" : 50,
     "position" : 1,
     "batch_size" : 64,
     "action_values" : [0.9,0.96,0.98,0.99,1,1.01,1.02,1.04,1.1]
 }
 state_size = 2
-harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "10T200 QRDQN FX MA",C=50, N=200,alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,n_hist_data=n_hist_data,n_hist_inputs=1,orderbook =False)
-tim = library.agents.baseAgents.TWAPAgent(1,"10T200 TWAP",11)
+harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "50T50 QRDQN FX MA",C=50, N=200,alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,n_hist_data=n_hist_data,n_hist_inputs=1,orderbook =False)
+tim = library.agents.baseAgents.TWAPAgent(1,"50T50 TWAP",11)
 agent = harry
 
 agent.learning_rate = 0.000025
 
-stock = library.market_modelsM.real_stock(merged,n_steps=200,n_train=50)
+stock = library.market_modelsM.real_stock(merged,n_steps=50,n_train=50)
 market = library.market_modelsM.market(stock,n_hist_data)
 market.k = 0.004
 market.b = 0.00004
