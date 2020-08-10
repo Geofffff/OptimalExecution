e96dc308a5a362d2df475ab58ff1dcca029be619
Toby Weston <tobyweston94@gmail.com>
2020-08-10 09:38:49 +0100

    cluster

--- cluster_sim_l1.py
+++ cluster_sim_l1.py
@@ -17,9 +17,9 @@ params = {
     "action_values" : [0.98,0.99,1,1.01,1.02]
 }
 state_size = 2
-harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "50T50 QRDQN FX MA",C=50, N=200,alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,n_hist_data=n_hist_data,n_hist_inputs=1,orderbook =False)
+harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "50T50 QRDQN FX2",C=50, N=200,alternative_target = True,UCB=True,UCBc = 100,tree_horizon = 4,n_hist_data=n_hist_data,n_hist_inputs=1,orderbook =False)
 tim = library.agents.baseAgents.TWAPAgent(1,"50T50 TWAP",11)
-agent = tim
+agent = harry
 
 agent.learning_rate = 0.000025
 
