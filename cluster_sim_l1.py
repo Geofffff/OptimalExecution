d4c029e9dc862ff9566b3d5a2de9ce61bf196121
Toby Weston <tobyweston94@gmail.com>
2020-08-22 17:16:50 +0100

    cluster

--- cluster_sim_l1.py
+++ cluster_sim_l1.py
@@ -38,7 +38,7 @@ params = {
     "num_trades" : 10,
     "position" : 1,
     "batch_size" : 32,
-    "action_values" : [0.99,1,1.01]
+    "action_values" : [0.5,0.9,0.98,0.99,1,1.01,1.02,1.1,2]
 }
 state_size = 2
 harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], "10T10 QRDQN CBTX4 MF",C=C, N=200,alternative_target = True,UCB=True,UCBc = UCBc,tree_horizon = 6,n_hist_data=n_hist_data,n_hist_inputs=4,orderbook =False)
