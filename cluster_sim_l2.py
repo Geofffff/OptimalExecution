b21929716189c0940ae0a1afd7daff7efa2529ac
Toby Weston <tobyweston94@gmail.com>
2020-09-06 18:36:49 +0100

    cluster

--- cluster_sim_l2.py
+++ cluster_sim_l2.py
@@ -46,7 +46,7 @@ params = {
 state_size = 3
 harry = library.agents.distAgentsWIP2.QRAgent(state_size, params["action_values"], f"{n_steps}T{n_steps} QRDQN BTX LO3",C=C, N=200,alternative_target = True,UCB=True,UCBc = UCBc,tree_horizon = n_steps,n_hist_data=n_hist_data,n_hist_inputs=7,orderbook =True)
 tim = library.agents.baseAgents.TWAPAgent(1,"TWAP",11)
-agent = tim
+agent = harry
 
 stock = library.market_modelsM.real_stock_lob(merged,n_steps=n_steps,n_train=30)
 market = library.market_modelsM.lob_market(stock,n_hist_data)
